1d38b51b5eebdb667ad6aed99b1f556fc8a933f1
drumath <2237k.drumath@gmail.com>
2022-03-19 02:46:31 +0900

    :sparkles: string->点群の処理を書いてみる

---
+++ Assets/PretiaMapConverter/Scripts/MapDataConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using PretiaArCloud.Pcx;
+using UnityEngine;
+using Utf8Json;
+
+namespace PretiaMapConverter
+{
+    public class MapDataConverter
+    {
+        public static (bool isSuccess, Exception err) TryGetPointCloudDataFromMap(
+            string mapData,
+            out List<Vector3> pointCloud)
+        {
+            var pretiaMapData = JsonSerializer.Deserialize<PretiaPointCloudData>(mapData);
+            
+            if (pretiaMapData != null)
+            {
+                pointCloud = pretiaMapData.GetVertices();
+                return (true, null);
+            }
+
+            pointCloud = null;
+            return (false, new InvalidOperationException("invalid map data"));
+        }
+    }
+}
