b40a2d5495c6c94293f796b4afc6942994d1d562
drumath <2237k.drumath@gmail.com>
2022-03-20 03:04:28 +0900

    :sparkles: ヘッダー部分の実装

---
+++ Assets/PretiaMapConverter/Scripts/PlyExporter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace PretiaMapConverter
+{
+    public class PlyExporter
+    {
+        public static byte[] ConvertPointCloudToPlyBinary(List<Vector3> pointCloud)
+        {
+            var str = "ply\nformat little_endian";
+            var bytes = System.Text.Encoding.UTF8.GetBytes(str);
+
+
+            foreach (var c in str.ToCharArray())
+            {
+                var code = (int)c;
+            }
+
+            return bytes;
+        }
+
+        private static byte[] CreatePlyHeader(int vertexCount)
+        {
+            var header = @$"ply
+format binary_little_endian 1.0
+element vertex {vertexCount}
+property float x
+property float y
+property float z
+end_header
+";
+            return Encoding.UTF8.GetBytes(header);
+        }
+    }
+}
---
+++ Assets/PretiaMapConverter/Tests/PlyExporterTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace PretiaMapConverter.Tests
+{
+    public class PlyExporterTest
+    {
+        [Test]
+        public void TestExporter()
+        {
+            PlyExporter.ConvertPointCloudToPlyBinary(null);
+            
+        }
+    }
+}
