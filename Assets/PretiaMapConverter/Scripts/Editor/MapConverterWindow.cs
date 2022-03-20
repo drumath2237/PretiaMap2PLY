8070c798530523745545cc69ab5b9d77ac42f360
drumath <2237k.drumath@gmail.com>
2022-03-21 01:08:47 +0900

    :sparkles: エディタのGUIを良い感じに配置

---
+++ Assets/PretiaMapConverter/Scripts/Editor/MapConverterWindow.cs
@@ -0,0 +1,45 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace PretiaMapConverter.Editor
+{
+    public class MapConverterWindow : EditorWindow
+    {
+        private string _mapFilePath = null;
+
+        [MenuItem("Pretia Map Converter/Export Wizard")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<MapConverterWindow>();
+            window.titleContent = new GUIContent("Export Wizard");
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.Space();
+            GUILayout.Label("Select map file");
+
+
+            EditorGUILayout.Space();
+            if (!string.IsNullOrEmpty(_mapFilePath))
+            {
+                EditorGUILayout.HelpBox($"Path:\n{_mapFilePath}", MessageType.None);
+            }
+            else
+            {
+                EditorGUILayout.HelpBox("Path is not assigned", MessageType.Error);
+            }
+            
+            if (GUILayout.Button("Select"))
+            {
+                _mapFilePath = EditorUtility.OpenFilePanelWithFilters("pretia map file", Application.dataPath,
+                    new[] { "Pretia Map", "map", "All files", "*" });
+            }
+
+
+            EditorGUILayout.Space();
+            GUILayout.Button("Export");
+        }
+    }
+}
